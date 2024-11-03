655ba5d9ff4eb27752efb77b1bb765a80794ceb8
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-11-03 18:12:12 +0530

    a

---
+++ lib/views/creater.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class createblog extends StatefulWidget {
+  const createblog({super.key});
+
+  @override
+  State<createblog> createState() => _createblogState();
+}
+
+class _createblogState extends State<createblog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
